71d3eb30d022a8d667ae314a3260e267f0cea097
Rakshith0808 <151515626+Rakshith0808@users.noreply.github.com>
2025-08-26 19:45:38 +0530

    Update api.js

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:4000/api', // backend base URL
+  baseURL: process.env.REACT_APP_BACKEND_URL || 'http://localhost:4000/api',
 });
 
 export default api;
